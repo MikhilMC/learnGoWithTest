2384845749604ffe6e25ddd0c7cfcd0e68c0a150
MikhilMC <mikhilmohan13@gmail.com>
2024-02-19 20:12:32 +0530

    repeating using for loop is added as a different function, refactored test file

--- iteration/repeat.go
+++ iteration/repeat.go
@@ -9,6 +9,15 @@ func Repeat(character string, repeatedCount int) string {
 	return strings.Repeat(character, repeatedCount)
 }
 
+func RepeatIterable(character string, repeatedCount int) string {
+	var repeated string
+	for i := 0; i < repeatedCount; i++ {
+		repeated += character
+	}
+	return repeated
+
+}
+
 func ExampleRepeat() {
 	character := "a"
 	count := 6
--- iteration/repeat_test.go
+++ iteration/repeat_test.go
@@ -3,16 +3,44 @@ package iteration
 import "testing"
 
 func TestRepeat(t *testing.T) {
-	repeated := Repeat("a", 5)
-	expected := "aaaaa"
+	t.Run("repeating a character by using string library function", func(t *testing.T) {
+		repeated := Repeat("a", 5)
+		expected := "aaaaa"
+		assertCorrectRepeat(t, repeated, expected)
+	})
 
-	if repeated != expected {
-		t.Errorf("expected %q but got %q", expected, repeated)
-	}
+	t.Run("fail", func(t *testing.T) {
+		repeated := Repeat("a", 6)
+		expected := "aaaaa"
+		assertCorrectRepeat(t, repeated, expected)
+	})
+}
+
+func TestRepeatIterable(t *testing.T) {
+	t.Run("repeating a character using iterable", func(t *testing.T) {
+		repeated := RepeatIterable("a", 5)
+		expected := "aaaaa"
+		assertCorrectRepeat(t, repeated, expected)
+	})
+
+	t.Run("fail", func(t *testing.T) {
+		repeated := RepeatIterable("a", 6)
+		expected := "aaaaa"
+		assertCorrectRepeat(t, repeated, expected)
+	})
+
 }
 
 func BenchmarkRepeat(b *testing.B) {
 	for i := 0; i < b.N; i++ {
 		Repeat("a", b.N)
+	}
+}
+
+func assertCorrectRepeat(t testing.TB, expected, repeated string) {
+	t.Helper()
+
+	if repeated != expected {
+		t.Errorf("expected %q but got %q", expected, repeated)
 	}
 }
