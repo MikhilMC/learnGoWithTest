fca13a462e858444ee004aa2fa74eab5a1268365
MikhilMC <mikhilmohan13@gmail.com>
2024-02-19 23:13:55 +0530

    Sum functions can now calculate the sum of any number of integers

--- arrays/sum_test.go
+++ arrays/sum_test.go
@@ -3,38 +3,33 @@ package main
 import "testing"
 
 func TestSum(t *testing.T) {
-	t.Run("Calculates the sum of numbers in the array", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 5}
+	t.Run("collection of 5 numbers", func(t *testing.T) {
+		numbers := []int{1, 2, 3, 4, 5}
 		got := Sum(numbers)
 		want := 15
 		assertCorrectSum(t, got, want, numbers)
 	})
 
-	t.Run("Incorrect sum", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 1}
+	t.Run("collection of any size", func(t *testing.T) {
+		numbers := []int{1, 2, 3}
 		got := Sum(numbers)
-		want := 15
+		want := 6
 		assertCorrectSum(t, got, want, numbers)
 	})
 }
 
 func TestSumOrdinaryForLoop(t *testing.T) {
-	t.Run("Calculates the sum of numbers in the array", func(t *testing.T) {
+	t.Run("collection of 5 numbers", func(t *testing.T) {
 		numbers := [5]int{1, 2, 3, 4, 5}
 		got := SumOrdinaryForLoop(numbers)
 		want := 15
-		assertCorrectSum(t, got, want, numbers)
-	})
-
-	t.Run("Incorrect sum", func(t *testing.T) {
-		numbers := [5]int{1, 2, 3, 4, 1}
-		got := SumOrdinaryForLoop(numbers)
-		want := 15
-		assertCorrectSum(t, got, want, numbers)
+		if got != want {
+			t.Errorf("got %d want %d given, %v", got, want, numbers)
+		}
 	})
 }
 
-func assertCorrectSum(t testing.TB, got, want int, numbers [5]int) {
+func assertCorrectSum(t testing.TB, got, want int, numbers []int) {
 	t.Helper()
 	if got != want {
 		t.Errorf("got %d want %d given, %v", got, want, numbers)
