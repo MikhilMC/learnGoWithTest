95cbd284f14851fcd4320ef86aabec08dd827785
MikhilMC <mikhilmohan13@gmail.com>
2024-02-22 18:29:36 +0530

    countdown with sleep

--- mocking/countdown.go
+++ mocking/countdown.go
@@ -4,18 +4,35 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"time"
 )
 
 const finalWord = "Go!"
 const countdownStart = 3
 
-func Countdown(out io.Writer) {
+type Sleeper interface {
+	Sleep()
+}
+
+type DefaultSleeper struct{}
+
+func (d *DefaultSleeper) Sleep() {
+	time.Sleep(1 * time.Second)
+}
+
+func Countdown(out io.Writer, sleeper Sleeper) {
+	for i := countdownStart; i > 0; i-- {
+		sleeper.Sleep()
+	}
+
 	for i := countdownStart; i > 0; i-- {
 		fmt.Fprintln(out, i)
 	}
+
 	fmt.Fprint(out, finalWord)
 }
 
 func main() {
-	Countdown(os.Stdout)
+	sleeper := &DefaultSleeper{}
+	Countdown(os.Stdout, sleeper)
 }
